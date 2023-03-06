c94f489a06384a36d7a85430e597e18b4b399dc4
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2023-03-06 11:21:39 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,2 +1,32 @@
-set = set([1,2,3,3,4,5,5,6,7,8,0,0])
-print(set)
+import random
+
+
+val= 2
+while val != 3:
+    choices = ["Rock", "Paper", "Scissors"]
+    user_input = input("Enter your choice: ")
+    choice = random.choice(choices)
+    if user_input == "Rock" and choice == "Paper":
+        print(f"you {user_input} wins")
+        print("Would you like to continue?")
+        proceed = input("Enter Yes/No")
+        if proceed == "Yes":
+            continue
+        elif proceed == "No":
+            break
+    elif choice == "Scissors" and user_input == "Paper":
+        print("Computer wins")
+        print("Would you like to continue?")
+        proceed = input("Enter Yes/No")
+        if proceed == "Yes":
+            continue
+        elif proceed == "No":
+            break
+    elif user_input == "Scissors" and choice == "Rock":
+        print("Computer wins")
+        print("Would you like to continue?")
+        proceed = input("Enter Yes/No")
+        if proceed == "Yes":
+            continue
+        elif proceed == "No":
+            break
